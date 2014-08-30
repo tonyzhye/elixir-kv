55d63735b9ed9cce10106544b52e5dd6dce650e4
Tony Ye <tony.zh.ye@gmail.com>
2014-08-30 17:28:31 +0800

    init the sample

---
+++ apps/kv/test/kv/registry_test.exs
@@ -0,0 +1,58 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  defmodule Forwarder do
+    use GenEvent
+
+    def handle_event(event, parent) do
+      send parent, event
+      {:ok, parent}
+    end
+  end
+
+  setup do
+    {:ok, sup} = KV.Bucket.Supervisor.start_link
+    {:ok, manager} = GenEvent.start_link
+    {:ok, registry} = KV.Registry.start_link(manager, sup)
+
+    GenEvent.add_handler(manager, Forwarder, self(), link: true)
+    {:ok, registry: registry}
+  end
+
+  test "spanwns buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "send events on create and crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    assert_receive {:create, "shopping", ^bucket}
+
+    Agent.stop(bucket)
+    assert_receive {:exit, "shopping", ^bucket}
+  end
+
+  test "remove bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Kill the bucket and wait for the notification
+    Process.exit(bucket, :shutdown)
+    assert_receive {:exit, "shopping", ^bucket}
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+end
